c3413002f735667c783a30cd383bc1b9e0521a85
khadar-oothuru <khadaroothuru@gmail.com>
2025-07-20 11:09:20 +0530

    latest push

--- smart_tissue_web/src/config/config.js
+++ smart_tissue_web/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
   // ws_web:"ws://localhost:8000",
 
   //API_URL :"http://127.0.0.1:8000/api"
-  API_URL: "https://trk.gyrfalconintelliedge.com:9003/api",
+ API_URL: "https://trk.gyrfalconintelliedge.com:9003/api",
 };
 
 export default config;
