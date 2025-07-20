279877c2e5dc227f3b8d8f3c314ea15c8fc56115
khadar-oothuru <khadaroothuru@gmail.com>
2025-07-20 11:10:51 +0530

    presenation  videos added of both mobile and web app

--- Smart_Tissue_Dispenser/config/config.js
+++ Smart_Tissue_Dispenser/config/config.js
@@ -1,10 +1,10 @@
 
 
 const config = {
-   //API_URL: "http://192.168.31.158:8000/api",
+  
  
   ws_web: "localhost:8000",
-  // ws: "192.168.31.101:8000",
+
   ws: "trk.gyrfalconintelliedge.com:9003",
 
   API_URL: "https://trk.gyrfalconintelliedge.com:9003/api",
